93266780933b64999f17e3b94dc65a43daa6d78c
NhanHuynh <huynhvannhancntt@gmail.com>
2025-05-21 22:01:02 +0700

    update code

--- src/main/java/com/example/shoponline/constants/Pages.java
+++ src/main/java/com/example/shoponline/constants/Pages.java
@@ -3,4 +3,5 @@ package com.example.shoponline.constants;
 public class Pages {
     public static final String ADMIN_PERFUMES = "admin-perfumes";
     public static final String ADMIN_ADD_PERFUME = "admin-add-perfume";
+    public static final String HOME = "home";
 }
